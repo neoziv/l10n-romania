50ab99f1434d7720d1eeabf515629c00319cfe5d
Lenovo <andrei.jorza@neoziv.ro>
2020-09-15 18:44:35 +0200

    updates from original branch
    updates from original branch

--- l10n_ro_city/__manifest__.py
+++ l10n_ro_city/__manifest__.py
@@ -1,10 +1,11 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright (C) 2020 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Romania - City",
     "summary": "Romania - City",
     "license": "AGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
